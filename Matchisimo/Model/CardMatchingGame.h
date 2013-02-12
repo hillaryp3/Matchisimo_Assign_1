a0d1a4b276d1ed0034bd753cd6c46901b99469d4
Hillary Parham <hillary.parham@gmail.com>
2013-02-12 07:53:33 -0500

    add 4 more cards and score

---
+++ Matchisimo/Model/CardMatchingGame.h
@@ -0,0 +1,23 @@
+//
+//  CardMatchingGame.h
+//  Matchisimo
+//
+//  Created by Hillary Parham on 2/12/13.
+//  Copyright (c) 2013 HJPCreations. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+- (id)initWithCardCount:(NSUInteger)cardCount
+              usingDeck:(Deck *)deck;
+
+-(void)flipCardAtIndex:(NSUInteger)index;
+
+-(Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) int score;
+
+@end
